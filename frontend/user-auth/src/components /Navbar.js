100a91c2aaed0ea398513ab023f3d65af7b3b268
torabi <torabi@torabis-MacBook-Air.local>
2024-07-27 03:53:34 -0700

    4Making the initial contract

---
+++ frontend/user-auth/src/components /Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link>
+        <h1>Auth DB</h1>
+      </Link>
+    </nav>
+  );
+}
